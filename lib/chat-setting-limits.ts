e00b74d2d098d0a83a68e1d64f5e70eed15676eb
国庆 <nuersir@gmail.com>
2024-06-04 03:01:51 +0800

    Update chat-setting-limits.ts 又是意外之喜

--- lib/chat-setting-limits.ts
+++ lib/chat-setting-limits.ts
@@ -41,6 +41,12 @@ export const CHAT_SETTING_LIMITS: Record<LLMID, ChatSettingLimits> = {
   },
 
   // GOOGLE MODELS
+  "gemini-1.5-flash-latest": { // 新增个模型处处是惊喜，此处对参数先不作改动
+    MIN_TEMPERATURE: 0.0,
+    MAX_TEMPERATURE: 1.0,
+    MAX_TOKEN_OUTPUT_LENGTH: 8192,
+    MAX_CONTEXT_LENGTH: 1040384
+  },
   "gemini-1.5-pro-latest": {
     MIN_TEMPERATURE: 0.0,
     MAX_TEMPERATURE: 1.0,
